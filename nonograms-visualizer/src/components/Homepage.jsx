63b0efda05a4658a7817dceeb712a47c2b322b40
mbarneto <mattdokn@gmail.com>
2022-09-02 07:32:28 -0400

    Page setup

--- nonograms-visualizer/src/components/Homepage.jsx
+++ nonograms-visualizer/src/components/Homepage.jsx
@@ -4,6 +4,18 @@ export default class Homepage extends Component {
   render() {
     return (
       <div className='body'>
+        <div className='topbar'>
+          <input id='search' type='text' autoComplete='off' />
+          <button id='start' aria-label='Start' />
+        </div>
+        <div id='content'>
+          <div id='left'>
+
+          </div>
+          <div id='right'>
+
+          </div>
+        </div>
       </div>
     )
   }
