fba64c7a05bc06be5aef8d5c21ad070575fb66af
mBarneto <4347791+m-barneto@users.noreply.github.com>
2022-10-07 20:00:01 -0400

    Moved html parsing logic to NonogramData constructor, allows returning the webdriver sooner

--- NonogramsVisualizerAPI/NonogramsVisualizerAPI/Controllers/NonogramsController.cs
+++ NonogramsVisualizerAPI/NonogramsVisualizerAPI/Controllers/NonogramsController.cs
@@ -5,6 +5,7 @@ using HtmlAgilityPack;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
+using NonogramsVisualizerAPI.Data;
 
 namespace NonogramsVisualizerAPI.Controllers {
     [ApiController]
@@ -41,36 +42,13 @@ namespace NonogramsVisualizerAPI.Controllers {
                 IWebElement nonogramTable = new WebDriverWait(driver, TimeSpan.FromSeconds(10)).Until(
                     elem => elem.FindElement(By.Id("nonogram_table"))
                 );
-                
-                string rowsLayerCountXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[2]/tbody/tr[2]/td[1]/table/tbody/tr[1]/td";
-                string colsLayerCountXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[2]/tbody/tr[1]/td[2]/table/tbody/tr";
-
-                string dimensionsXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[1]/tbody/tr/td[1]";
-
-                string dimText = FindElement(driver, dimensionsXPath).Text;
-                string dimensions = dimText.Substring("Size: ".Length);
-
-                int colCount = int.Parse(dimensions.Split('x')[0]);
-                int rowCount = int.Parse(dimensions.Split('x')[1]);
-
-                int rowsLayerCount = FindElements(driver, rowsLayerCountXPath).Count;
-                int colsLayerCount = FindElements(driver, colsLayerCountXPath).Count;
-
-                Console.WriteLine($"Rows: {rowCount}\nRow Layers: {rowsLayerCount}\nCols: {colCount}\nCol Layers: {colsLayerCount}");
 
                 // Now that we've navigated to the site and the nonogram is populated, we can scrape the html values without holding onto the webdriver
+                HtmlDocument doc = new HtmlDocument();
+                doc.LoadHtml(driver.PageSource);
+                NonogramData nonogram = new NonogramData(doc);
 
                 NonogramsDriver.ReturnControl();
-
-                // scrap the table
-
-                HtmlDocument doc = new HtmlDocument();
-                doc.LoadHtml(data);
-
-                string nonogramData = doc.DocumentNode.SelectSingleNode("//div[@class='" + "content" + "']").ChildNodes[17].InnerText;
-                string jsonData = nonogramData.Substring(nonogramData.IndexOf("var d=") + 6, nonogramData.Length - 9);
-
-                List<List<int?>?>? nonogram = JsonConvert.DeserializeObject<List<List<int?>?>?>(jsonData);
 
                 return Json(nonogram);
 
--- NonogramsVisualizerAPI/NonogramsVisualizerAPI/Data/NonogramData.cs
+++ NonogramsVisualizerAPI/NonogramsVisualizerAPI/Data/NonogramData.cs
@@ -8,7 +8,58 @@ namespace NonogramsVisualizerAPI.Data {
         public List<int> rowData, columnData;
 
         public NonogramData(HtmlDocument doc) {
+            string rowsLayerCountXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[2]/tbody/tr[2]/td[1]/table/tbody/tr[1]/td";
+            string colsLayerCountXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[2]/tbody/tr[1]/td[2]/table/tbody/tr";
 
+            string dimensionsXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[1]/tbody/tr/td[1]";
+
+            string dimText = doc.DocumentNode.SelectSingleNode(dimensionsXPath).InnerText;
+            string dimensions = dimText.Substring("Size: ".Length);
+
+            columns = int.Parse(dimensions.Split('x')[0]);
+            rows = int.Parse(dimensions.Split('x')[1]);
+
+            rowLayers = doc.DocumentNode.SelectNodes(rowsLayerCountXPath).Count;
+            columnLayers = doc.DocumentNode.SelectNodes(colsLayerCountXPath).Count;
+
+            rowData = new List<int>();
+            columnData = new List<int>();
+
+            string rowDataXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[2]/tbody/tr[1]/td[2]/table/tbody/tr";
+
+            foreach (var row in doc.DocumentNode.SelectNodes(rowDataXPath)) {
+                foreach (var tile in row.ChildNodes) {
+                    if (tile.ChildNodes.Count > 0) {
+                        Console.WriteLine(tile.ChildNodes[0].InnerText);
+                        if (tile.ChildNodes[0].InnerText.Equals("&nbsp;")) {
+                            rowData.Add(-1);
+                        } else {
+                            rowData.Add(int.Parse(tile.ChildNodes[0].InnerText));
+                        }
+                    } else {
+                        Console.WriteLine("Iterated over tile without child element");
+                    }
+                }
+            }
+
+            string columnDataXPath = "/html/body/table/tbody/tr[1]/td[2]/div[2]/table[2]/tbody/tr[2]/td[1]/table/tbody/tr";
+
+            foreach (var col in doc.DocumentNode.SelectNodes(columnDataXPath)) {
+                foreach (var tile in col.ChildNodes) {
+                    if (tile.ChildNodes.Count > 0) {
+                        Console.WriteLine(tile.ChildNodes[0].InnerText);
+                        if (tile.ChildNodes[0].InnerText.Equals("&nbsp;")) {
+                            columnData.Add(-1);
+                        }
+                        else {
+                            columnData.Add(int.Parse(tile.ChildNodes[0].InnerText));
+                        }
+                    }
+                    else {
+                        Console.WriteLine("Iterated over tile without child element");
+                    }
+                }
+            }
         }
     }
 }
